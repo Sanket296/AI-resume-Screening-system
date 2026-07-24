0171c6a27fe501e22e0aaa8f19ec8a44de803ff2
Sanket296 <sanketneekhra296@gmail.com>
2026-07-24 18:40:36 +0530

    adding links

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://ai-resume-screening-system-86sw.vercel.app/",
     withCredentials: true
 })
 
