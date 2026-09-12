d7fd065d56c674d47e8bae0f298236df3f4ef8aa
Sanket296 <sanketneekhra296@gmail.com>
2026-09-12 17:13:14 +0530

    refactor: Update base URLs for API services

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "http://localhost:3000" || process.env.Backend_BASE_URL,
     withCredentials: true
 })
 
